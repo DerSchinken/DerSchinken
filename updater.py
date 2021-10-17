f183c1c3517f2e92018d4432be8a669a454414d2
DerSchinken <paulhippler21@gmail.com>
2021-10-17 12:52:12 +0200

    removed old code

--- updater.py
+++ updater.py
@@ -1,23 +1,3 @@
-"""from scraper import get_total_additions_deletions
-from bs4 import BeautifulSoup
-import requests
-
-with open("README_template.md", "r") as f:
-    readme_template = f.read()
-
-total_additions = 0
-total_deletions = 0
-
-repos = requests.get("https://github.com/DerSchinken?tab=repositories")
-repos = BeautifulSoup(repos.content, "html.parser").find_all(itemprop="name codeRepository")
-for repo in repos:
-    gtad = get_total_additions_deletions("DerSchinken", repo.string.strip()).values()
-    total_additions += gtad["total_additions"]
-    total_deletions += gtad["total_deletions"]
-
-with open("README.md", "w") as f:
-    f.write(readme_template.format(adds=total_additions, removes=total_deletions))
-"""
 import os
 from github import Github
 
