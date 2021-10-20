476d5ac0f66e774f9ad11f4cd6a124cba17aa1aa
DerSchinken <paulhippler21@gmail.com>
2021-10-20 22:23:13 +0200

    new way of getting code freq

--- updater.py
+++ updater.py
@@ -12,9 +12,9 @@ def get_total_additions_deletions(username: str) -> dict:
     total_deletions = 0
 
     for repo in g.get_user(username).get_repos():
-        for commit in repo.get_commits():
-            total_additions += commit.stats.additions
-            total_deletions += commit.stats.deletions
+        for weekly_code_frequency in repo.get_stats_code_frequency():
+            total_additions += weekly_code_frequency.additions
+            total_deletions += weekly_code_frequency.deletions
 
     return {"total_additions": total_additions, "total_deletions": total_deletions}
 
